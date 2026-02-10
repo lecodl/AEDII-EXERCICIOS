477632ec6bbbd81f49c1bef2a94f6cf09ba0a985
Leonardo Lima <leolimach1905@gmail.com>
2026-02-10 08:51:37 -0300

    Create lista01exercicio01.cs

---
+++ lista-01/lista01exercicio01.cs
@@ -0,0 +1,28 @@
+// Determine as raízes de uma equação de 2º grau: ax2 + bx + c = 0 (recordar que o
+// discriminante Δ = b2 – 4ac, e que a raiz r = (–b ± √Δ)/2a).
+
+using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o valor de a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de b: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite o valor de c: ");
+        double c = double.Parse(Console.ReadLine());
+
+        Complex delta = new Complex(b*b-4*a*c, 0);
+
+        Complex r1 = (-b + Complex.Sqrt(delta))/(2*a);
+        Complex r2 = (-b - Complex.Sqrt(delta))/(2*a);
+
+        Console.WriteLine($"Raiz 1: {r1}");
+        Console.WriteLine($"Raiz 2: {r2}");
+    }
+}
