de00931b0b088ec46820772113212bc608eaa64d
Leonardo Lima <leolimach1905@gmail.com>
2026-02-12 12:11:13 -0300

    Update lista01exercicio02.cs

--- lista-01/lista01exercicio02.cs
+++ lista-01/lista01exercicio02.cs
@@ -4,26 +4,24 @@ using System;
 
 class Program
 {
-    static void Main()
-    {
-        Console.Write("Digite x1: ");
-        double x1 = double.Parse(Console.ReadLine());
-        Console.Write("Digite y1: ");
-        double y1 = double.Parse(Console.ReadLine());
-        Console.Write("Digite z1: ");
-        double z1 = double.Parse(Console.ReadLine());
-
-        Console.Write("Digite x2: ");
-        double x2 = double.Parse(Console.ReadLine());
-        Console.Write("Digite y2: ");
-        double y2 = double.Parse(Console.ReadLine());
-        Console.Write("Digite z2: ");
-        double z2 = double.Parse(Console.ReadLine());
-
-        double distancia = Math.Sqrt(Math.Pow(x2-x1, 2) +
-                                     Math.Pow(y2-y1, 2) +
-                                     Math.Pow(z2-z1, 2));
-
-        Console.WriteLine($"Distância: {distancia}");
-    }
-}
+	static void Main()
+	{
+		Console.WriteLine("Digite x1");
+		double x1 = double.Parse(Console.ReadLine());
+		Console.WriteLine("Digite y1");
+		double y1 = double.Parse(Console.ReadLine());
+		Console.WriteLine("Digite z1");
+		double z1 = double.Parse(Console.ReadLine());
+		
+		Console.WriteLine("Digite x2");
+		double x2 = double.Parse(Console.ReadLine());
+		Console.WriteLine("Digite y2");
+		double y2 = double.Parse(Console.ReadLine());
+		Console.WriteLine("Digite z2");
+		double z2 = double.Parse(Console.ReadLine());
+		
+		double distancia = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
+		
+		Console.WriteLine($"distância dos dois pontos: {distancia}");
+	}
+}	
